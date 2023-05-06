64ac2580c5b103fbb8be6aa5ff580a8c1e6bd262
Dinesh Kumar G <gdineshda@gmail.com>
2023-05-06 15:32:16 +0530

    Added Semester Component

---
+++ src/Semester.jsx
@@ -0,0 +1,32 @@
+import { Card, CardContent, Box, Grid, Typography } from '@mui/material';
+import Grade from './Grade';
+import React from 'react';
+
+export default function Semester({ semesterName, subjects }) {
+	return (
+		<Card sx={{ minWidth: "2.5rem", backgroundColor: "#434c5e" }}>
+			<CardContent>
+				<Grid>
+					<Typography sx={{
+						textAlign: "center",
+						color: "#d8dee9",
+						fontFamily: "Barlow Condensed",
+						fontSize: 30,
+					}}>
+						{semesterName}
+					</Typography>
+					<Grid display="flex"
+						sx={{
+							height: "auto",
+							flexDirection: "column",
+						}}
+					>
+						{subjects.map((x) =>
+							<Grade label={x} labelid={x + '-label'} id={x} />
+						)}
+					</Grid>
+				</Grid>
+			</CardContent>
+		</Card>
+	);
+}
