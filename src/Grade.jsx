39e184eb61e6eac2bbab2443659c0b4673f3e6f2
Dinesh Kumar G <gdineshda@gmail.com>
2023-05-06 15:32:03 +0530

    Added Grade Component

---
+++ src/Grade.jsx
@@ -0,0 +1,51 @@
+import * as React from 'react';
+import { Typography, Select, FormControl, InputLabel, MenuItem, Box } from '@mui/material';
+
+export default function Grade({ labelid, id, label }) {
+	const [grade, setGrade] = React.useState('O');
+
+	const handleChange = (event) => {
+		setGrade(event.target.value);
+	};
+
+	return (
+		<Box display="flex" alignItems="center">
+			<FormControl sx={{ m: 1, width: 200 }} size="small">
+				<InputLabel id={labelid}>{label}</InputLabel>
+				<Select
+					labelId={labelid}
+					id={id}
+					value={grade}
+					label={label}
+					onChange={handleChange}
+					sx={{
+						color: "#d8dee9"
+					}}
+				>
+					<MenuItem value={10}>
+						<Typography>O</Typography>
+					</MenuItem>
+					<MenuItem value={9}>
+						<Typography>A+</Typography>
+					</MenuItem>
+					<MenuItem value={8}>
+						<Typography>A</Typography>
+					</MenuItem>
+					<MenuItem value={7}>
+						<Typography>B+</Typography>
+					</MenuItem>
+					<MenuItem value={6}>
+						<Typography>B</Typography>
+
+					</MenuItem>
+					<MenuItem value={5}>
+						<Typography>C</Typography>
+					</MenuItem>
+					<MenuItem value={0}>
+						<Typography>U</Typography>
+					</MenuItem>
+				</Select>
+			</FormControl>
+		</Box>
+	);
+}
